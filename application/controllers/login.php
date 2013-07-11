b34c7487509db723ed286a971a448e2fd4840c75
unknown <selimsong@SHA0W0123270.ap.corp.jwt.com>
2013-07-11 14:35:19 +0800

    'm'

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -15,7 +15,7 @@ class Login extends CI_Controller {
 	
 	public function validate(){
 		 print_r($_POST);
-		
+		    
 	}
 	
 	
--- application/views/header.php
+++ application/views/header.php
@@ -6,5 +6,6 @@
 	<link rel='stylesheet' id='wp-admin-css'  href='http://localhost/wordpress/wp-admin/css/wp-admin.min.css?ver=3.5.2' type='text/css' media='all' />
 <link rel='stylesheet' id='buttons-css'  href='http://localhost/wordpress/wp-includes/css/buttons.min.css?ver=3.5.2' type='text/css' media='all' />
 <link rel='stylesheet' id='colors-fresh-css'  href='http://localhost/wordpress/wp-admin/css/colors-fresh.min.css?ver=3.5.2' type='text/css' media='all' />
+<link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/styles.css" media="screen" />
 <meta name='robots' content='noindex,nofollow' />
 </head>
