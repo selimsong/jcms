603b9fc1a78d598339670cf7cf19514de65de9d0
selim song <491497047@qq.com>
2013-07-14 21:49:45 +0800

    m

--- application/backend/views/header.php
+++ application/backend/views/header.php
@@ -3,9 +3,9 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>backend - jcms &rsaquo; Log In</title>
-	<link rel='stylesheet' id='wp-admin-css'  href='http://localhost/wordpress/wp-admin/css/wp-admin.min.css?ver=3.5.2' type='text/css' media='all' />
 <link rel='stylesheet' id='buttons-css'  href='http://localhost/wordpress/wp-includes/css/buttons.min.css?ver=3.5.2' type='text/css' media='all' />
 <link rel='stylesheet' id='colors-fresh-css'  href='http://localhost/wordpress/wp-admin/css/colors-fresh.min.css?ver=3.5.2' type='text/css' media='all' />
 <link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/styles.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/wp-admin.css" media="screen" />
 <meta name='robots' content='noindex,nofollow' />
 </head>
