3386c61e34d688e341948ef2b64d684bffdb7bee
selim song <491497047@qq.com>
2013-07-15 19:03:51 +0800

    m

--- application/backend/views/header.php
+++ application/backend/views/header.php
@@ -3,11 +3,10 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>backend - jcms &rsaquo; Log In</title>
-<link rel='stylesheet' id='buttons-css'  href='http://localhost/wordpress/wp-includes/css/buttons.min.css?ver=3.5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='colors-fresh-css'  href='http://localhost/wordpress/wp-admin/css/colors-fresh.min.css?ver=3.5.2' type='text/css' media='all' />
 <link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/styles.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/wp-admin.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/backend.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/colors-fresh.css" media="screen" />
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <meta name='robots' content='noindex,nofollow' />
 <style type="text/css">
