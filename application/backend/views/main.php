d6477bd1f17f9ef82e8f30ca10145c704b725deb
selim song <491497047@qq.com>
2013-07-15 10:50:37 +0800

    m

--- application/backend/views/main.php
+++ application/backend/views/main.php
@@ -1,5 +1,5 @@
 <div class="container">
-	<h3>UCenter 统计信息</h3>
+	<h3> 统计信息</h3>
 	<ul class="memlist fixwidth">
 		<li><em><a href="admin.php?m=app&a=ls">应用总数</a>:</em>1</li>
 		<li><em><a href="admin.php?m=user&a=ls">用户总数</a>:</em>2</li>
@@ -14,66 +14,20 @@
 	
 	<h3>系统信息</h3>
 	<ul class="memlist fixwidth">
-		<li><em>UCenter 程序版本:</em>UCenter 1.6.0 Release 20110501 <a href="http://www.discuz.net/forumdisplay.php?fid=151" target="_blank">查看最新版本</a> 
-		<li><em>操作系统及 PHP:</em>WINNT / PHP v5.4.16</li>
+		
 		<li><em>服务器软件:</em>Apache/2.4.4 (Win32) OpenSSL/0.9.8y PHP/5.4.16</li>
 		<li><em>MySQL 版本:</em>5.5.32</li>
 		<li><em>上传许可:</em>2M</li>
 		<li><em>当前数据库尺寸:</em>395.61 KB</li>		
-		<li><em>主机名:</em>localhost (127.0.0.1:80)</li>
-		<li><em>magic_quote_gpc:</em>Off</li>
-		<li><em>allow_url_fopen:</em>On</li>		
+			
 	</ul>
-	<h3>UCenter 开发团队</h3>
+	<h3>开发团队</h3>
 	<ul class="memlist fixwidth">
 		<li>
 			<em>版权所有:</em>
-			<em class="memcont"><a href="http://www.comsenz.com" target="_blank">&#x5317;&#x4EAC;&#x5EB7;&#x76DB;&#x65B0;&#x521B;&#x79D1;&#x6280;&#x6709;&#x9650;&#x8D23;&#x4EFB;&#x516C;&#x53F8;</a></em>
+			<em class="memcont"><a href="" target="_blank">jcms</a></em>
 		</li>
-		<li>
-			<em>总策划兼项目经理:</em>
-			<em class="memcont"><a href="http://www.discuz.net/space.php?uid=1" target="_blank">&#x6234;&#x5FD7;&#x5EB7; (Kevin 'Crossday' Day)</a></em>
-		</li>
-		<li>
-			<em>开发团队:</em>
-			<em class="memcont">
-				<a href="http://www.discuz.net/space.php?uid=859" target="_blank">Hypo 'cnteacher' Wang</a>,
-				<a href="http://www.discuz.net/space.php?uid=80629" target="_blank">Ning 'Monkey' Hou</a>,				
-				<a href="http://www.discuz.net/space.php?uid=875919" target="_blank">Jie 'tom115701' Zhang</a>
-			</em>
-		</li>
-		<li>
-			<em>安全团队:</em>
-			<em class="memcont">
-				<a href="http://www.discuz.net/space.php?uid=859" target="_blank">Hypo 'cnteacher' Wang</a>,
-				<a href="http://www.discuz.net/space.php?uid=492114" target="_blank">Liang 'Metthew' Xu</a>,
-				<a href="http://www.discuz.net/space.php?uid=285706" target="_blank">Wei (Sniffer) Yu</a>
-			</em>
-		</li>
-		<li>
-			<em>界面与用户体验团队:</em>
-			<em class="memcont">
-				<a href="http://www.discuz.net/space.php?uid=294092" target="_blank">Fangming 'Lushnis' Li</a>,
-				<a href="http://www.discuz.net/space.php?uid=717854" target="_blank">Ruitao 'Pony.M' Ma</a>
-			</em>
-		</li>
-		<li>
-			<em>感谢贡献者:</em>
-			<em class="memcont">
-				<a href="http://www.discuz.net/space.php?uid=122246" target="_blank">Heyond</a>
-			</em>
-		</li>
-		<li>
-			<em>公司网站:</em>
-			<em class="memcont"><a href="http://www.comsenz.com" target="_blank">http://www.Comsenz.com</a></em>
-		</li>
-		<li>
-			<em>产品官方网站:</em>
-			<em class="memcont"><a href="http://www.discuz.com" target="_blank">http://www.Discuz.com</a></em>
-		</li>
-		<li>
-			<em>产品官方论坛:</em>
-			<em class="memcont"><a href="http://www.discuz.net" target="_blank">http://www.Discuz.net</a></em>
-		</li>
+		
+
 	</ul>
 </div>
