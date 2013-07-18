b969233d7cde45f921d8352a718350e5630efbe7
selim song <491497047@qq.com>
2013-07-18 11:27:51 +0800

    m

--- application/backend/models/post_model.php
+++ application/backend/models/post_model.php
@@ -5,6 +5,7 @@ class post_model  extends CI_Model{
 	function save(){
 		$this->title   = $_POST['post_title'];
 		$this->content = $_POST['post_content'];
+		$this->cate_id = $_POST['category'];
 		$this->date    = date("y-m-d H:i:s" ,time()); 
 		$this->db->insert('posts', $this);
 	}
