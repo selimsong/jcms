ce6fd0c4ac940ae9ed7018cb9109269d272a4db8
selim song <491497047@qq.com>
2013-07-18 18:34:17 +0800

    m

--- application/backend/views/header.php
+++ application/backend/views/header.php
@@ -1,22 +1,10 @@
 <!DOCTYPE html>
-	<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
-	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>backend - jcms &rsaquo; Log In</title>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>backend - jcms &rsaquo; Log In</title>
 <link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/wp-admin.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/backend.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="<?php echo  base_url();  ?>css/colors-fresh.css" media="screen" />
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<meta name='robots' content='noindex,nofollow' />
-<style type="text/css">
-.menu{ position:relative; z-index:20; margin:0px; padding:0px;
-}
-	.menu ul{ position:absolute; top:10px; right:-1px !important; right:-2px; list-style:none; width:150px;
-	      }
-		.menu li{ margin:3px 0; *margin:1px 0; height:auto !important; height:24px; overflow:hidden; font-size:14px; font-weight:700; }
-		.menu li a{ display:block; margin-right:2px; padding:3px 0 2px 30px; *padding:4px 0 2px 30px; border:1px solid #F2F9FD;  color:#666; }
-			.menu li a:hover{ text-decoration:none; margin-right:0; border:1px solid #B5CFD9; border-right:1px solid #FFF; background:#FFF; }
-		.menu li a.tabon{ text-decoration:none; margin-right:0; border:1px solid #B5CFD9; border-right:1px solid #FFF;  color:#2366A8; }
-.footer{ position:absolute; z-index:10; right:13px; bottom:0; padding:5px 0; line-height:150%;  font-family:Arial, sans-serif; font-size:10px; -webkit-text-size-adjust: none; }
-</style>
 </head>
