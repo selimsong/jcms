65b8340c7adc2ec0849853529ab922ab80a08478
selim song <491497047@qq.com>
2013-07-19 17:47:04 +0800

    m

--- application/backend/views/setting.php
+++ application/backend/views/setting.php
@@ -17,31 +17,14 @@
 			<form action="<?php echo  base_url();  ?>admin/user/save" method="post">
 				<table class="opt">
 					<tr>
-						<th colspan="1">Add New User</th>
+						<th colspan="1">General Settings</th>
 					</tr>
 					<tr>
-						<td>Username:</td>
-						<td><input type="text" class="txt" name="user_name" autocomplete="off"  value="" /></td>
+						<td>Site Title:</td>
+						<td><input type="text" class="txt" name="site_title" autocomplete="off"  value="" /></td>
 					</tr>
 
-				    <tr>
-						<td>Role:</td>
-						<td>
-							<select name="role">
-							   
-							</select>
-						</td>
-					</tr>
-					
-					<tr>
-						<td>E-mail:</td>
-						<td><input type="text" class="txt" name="user_email" autocomplete="off" value="" /></td>
-					</tr>
-                    
-                    <tr>
-						<td>Password:</td>
-						<td><input type="password" class="txt" name="user_password" autocomplete="off" value="" /></td>
-					</tr>
+
 					
 				</table>
 				<div class="opt"><input type="submit" name="submit" value=" 提 交 " class="btn" tabindex="3" /></div>
